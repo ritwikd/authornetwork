65284f34f44d30f1b66508aed71ab70f479c803f
sld-generic <sld-generic@turf.ece>
2015-06-22 11:06:20 -0400

    Initial code.

---
+++ extractdata/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'sld-generic'
---
+++ extractdata/main.py
@@ -0,0 +1,31 @@
+import csv
+__author__ = 'sld-generic'
+
+
+author_fields_csv_raw = []
+
+#Read CSV file
+with open("data.csv", "r") as publication_data:
+    reader = csv.reader(publication_data)
+    #Process by row
+    for row in reader:
+        
+        author_fields_csv_raw.append(row[1])
+
+author_fields_csv_raw = author_fields_csv_raw[2:]
+
+authors_field_csv_separated = []
+
+for author_field_csv in author_fields_csv_raw:
+    author_names = author_field_csv.split(';')
+    for author_name in author_names:
+        if author_name not in authors_field_csv_separated:
+            authors_field_csv_separated.append(author_name)
+
+output_author_nodes_csv = open("output/author_nodes.csv", "w+")
+
+for author in authors_field_csv_separated:
+    output_author_nodes_csv.write(author + ",")
+output_author_nodes_csv.close()
+
+
