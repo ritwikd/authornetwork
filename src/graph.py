da7c796d4fbc46d7631e29edb4503e0073b480f1
Ritwik Dutta <ritzymail@gmail.coml>
2015-06-23 18:25:10 -0400

    Making code more efficient.

--- src/graph.py
+++ src/graph.py
@@ -24,73 +24,80 @@ csv_metadata = []
 csv_author_fields = []
 csv_author_fields = csv_author_fields[2:]
 names_separated = []
-graph_node_ids = {}
 graph_edges = {}
 
-# Read CSV file and save metadata
+# Read CSV file
 for filename in input_data_files:
     with open(input_data_path + filename, "r") as citation_data:
+
+        # Get and save metadata
         data_reader = csvreader(citation_data)
         for citation in data_reader:
-            csv_author_fields.append(citation[1])
+            csv_author_fields.append(citation[1].strip())
             csv_metadata.append(citation)
 
 # Create nodes for author
 for field in csv_author_fields:
+
+    # Get names and strip whitespace
     field_names = field.split(input_data_delimiter)
+    for i in range(len(field_names)):
+        field_names[i] = field_names[i].replace(' ', '')
+
+    # Add new authors to name array and create ID
     for name in field_names:
-        name = name.strip()
-        # Add new authors to name array and create ID
         if name not in names_separated:
-            names_separated.append(name)
-            graph_node_ids[name] = len(names_separated)
+            names_separated.append(name.replace(' ', ''))
+
+# Count total names
 total_names = len(names_separated)
 
-# Open edge file handler and related vars
-csv_nodes_fp = output_data_path + input_data_name + "nodes.csv"
-csv_nodes_fh = open(csv_nodes_fp, "w+")
-csv_nodes_delimiter = ";"
-# Write nodes to file in CSV format
-csv_nodes_fh.write("Label" + csv_nodes_delimiter + "Id\n")
-for name in names_separated:
-    csv_nodes_fh.write(name + csv_nodes_delimiter + str(graph_node_ids[name]) + "\n")
-csv_nodes_fh.close()
-
-# Create empty edge array for each author
-for name in names_separated:
-    graph_edges[graph_node_ids[name]] = []
+# Create graph object
+graph = nx.DiGraph()
 
 # Create edges between co-authors
 total_edges = 0
-for field in csv_author_fields:
-    field_names = field.split(';')
-    for edge_start_name in field_names:
-        name_index = field_names.index(edge_start_name)
-        other_names = field_names[:name_index] + field_names[name_index + 1:]
-        for edge_end_name in other_names:
-            # Create ids for start and end of edge
-            edge_start_id = graph_node_ids[edge_start_name.strip()]
-            edge_end_id = graph_node_ids[edge_end_name.strip()]
-            # Check if edge exists currently and if not create it
-            if edge_end_id not in graph_edges[edge_start_id]:
-                if edge_start_id not in graph_edges[edge_end_id]:
-                    graph_edges[edge_start_id].append(edge_end_id)
-                    total_edges += 1
+for field in csv_author_fields[2:]:
 
-# Open edge file handler and related vars
-csv_edges_fp = output_data_path + input_data_name + "edges.csv"
-csv_edges_fh = open(csv_edges_fp, "w+")
-csv_edges_delimiter = ","
-# Write edges to file in CSV format
-csv_edges_fh.write("Source" + csv_edges_delimiter + "Target\n")
-for edge_start_id in graph_edges.keys():
-    for edge_end_id in graph_edges[edge_start_id]:
-        csv_edges_fh.write(str(edge_start_id) + csv_edges_delimiter + str(edge_end_id) + "\n")
-csv_edges_fh.close()
+    # Get names and strip whitespace
+    field_names = field.split(input_data_delimiter)
+    for i in range(len(field_names)):
+        field_names[i] = field_names[i].replace(' ', '')
+
+    # Only multi author papers
+    if len(field_names) > 1:
+        for edge_start_name in field_names:
+
+            # Get edge start and possible edge endpoints
+            edge_start_name = edge_start_name.replace(' ', '')
+            name_index = field_names.index(edge_start_name)
+            other_names = field_names[:name_index] + field_names[name_index + 1:]
+
+            # Check all possible endpoints
+            for edge_end_name in other_names:
+
+                # Strip whitespace
+                edge_end_name = edge_end_name.replace(' ', '')
+
+                # Create empty edge array for nodes
+                if edge_start_name not in graph_edges.keys():
+                    graph_edges[edge_start_name] = []
+                if edge_end_name not in graph_edges.keys():
+                    graph_edges[edge_end_name] = []
+
+                # Create edges that are nonexistent in both directions
+                if edge_end_name not in graph_edges[edge_start_name.strip()]:
+                    if edge_start_name not in graph_edges[edge_end_name]:
+                        graph.add_edge(edge_start_name, edge_end_name)
+                        total_edges += 1
+
+
+# Write gexf file for graph
+edges_gexf_fp = output_data_path + input_data_name + ".gexf"
+nx.write_gexf(graph, edges_gexf_fp)
 
 # Write log statements to file
 csv_log_fp = output_data_path + input_data_name + "log.txt"
 csv_log_fh = open(csv_log_fp, "w+")
-csv_log_fh.write(str(total_names) + " author names successfully written to " + csv_nodes_fp + " in CSV format.\n")
-csv_log_fh.write(str(total_edges) + " author edges successfully written to " + csv_edges_fp + " in CSV format.")
+csv_log_fh.write(str(total_edges) + " author edges successfully written to " + edges_gexf_fp + " in GEXF format")
 csv_log_fh.close()
