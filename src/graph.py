328283727aaaa940ab06ebfbfc45b6e32603af39
Ritwik Dutta <ritzymail@gmail.coml>
2015-06-23 18:49:08 -0400

    Cleaning code.

--- src/graph.py
+++ src/graph.py
@@ -1,77 +1,87 @@
 __author__ = 'Ritwik Dutta'
 
 # Imports for various required modules
-from csv import reader as csvreader
+from csv import reader as csv_parse
 from sys import argv as cmd_args
 from os import path, makedirs, listdir
 import networkx as nx
 
-# Get info from CLI args
-input_data_path = cmd_args[1]
-input_data_delimiter = cmd_args[3]
-input_data_name = input_data_path.split("/")[3]
+# CLI args
+input_path  = cmd_args[1]
+input_delim = cmd_args[3]
+input_name  = input_path.split("/")[3]
+
 # Check for name arg
 if len(cmd_args) == 5:
-    input_data_name = cmd_args[4]
+    input_name = cmd_args[4]
+
 # Generate full I/O paths and create folders if needed
-input_data_files = listdir(input_data_path)
-output_data_path = cmd_args[2] + input_data_name + "/"
+input_data_files = listdir(input_path)
+output_data_path = cmd_args[2] + input_name + "/"
 if not path.isdir(output_data_path):
     makedirs(output_data_path)
 
-# Create variables to hold graph information
-csv_metadata = []
-csv_author_fields = []
-csv_author_fields = csv_author_fields[2:]
-names_separated = []
-graph_edges = {}
+# Program vars
+metadata    = [] # CSV metadata
+author_flds = [] # Author fields
+authors     = [] # Author names
+edges       = {} # Graph edges
 
-# Read CSV file
+# Get all files in input dir
 for filename in input_data_files:
-    with open(input_data_path + filename, "r") as citation_data:
 
-        # Get and save metadata
-        data_reader = csvreader(citation_data)
-        for citation in data_reader:
-            csv_author_fields.append(citation[1].strip())
-            csv_metadata.append(citation)
+    # Read each file
+    csv_file = open(input_path + filename, "r")
+    with csv_file as current_file:
 
-# Create nodes for author
-for field in csv_author_fields:
+        # Parse file into records
+        records = csv_parse(current_file)
 
-    # Get names and strip whitespace
-    field_names = field.split(input_data_delimiter)
-    for i in range(len(field_names)):
-        field_names[i] = field_names[i].replace(' ', '')
+        # Save metadata and create author nodes
+        for record in records:
 
-    # Add new authors to name array and create ID
-    for name in field_names:
-        if name not in names_separated:
-            names_separated.append(name.replace(' ', ''))
+            # Save metadata
+            metadata.append(record)
+            author_field = record[1].split(input_delim)
 
-# Count total names
-total_names = len(names_separated)
+
+            # Parse names
+            for i in range(len(author_field)):
+
+                #Strip whitespace
+                author_field[i] = author_field[i].replace(' ', '')
+
+                # Create node and empty edge set
+                if author_field[i] not in authors:
+                    authors.append(author_field[i])
+                    edges[author_field[i]] = []
+
+            # Save stripper
+            author_flds.append(author_field)
+
+# Remove fluff from fields
+author_flds = author_flds[2:]
 
 # Create graph object
 graph = nx.DiGraph()
 
-# Create edges between co-authors
-total_edges = 0
-for field in csv_author_fields[2:]:
+# Create and count edges between co-authors
+edge_number = 0
+for field in author_flds:
 
     # Get names and strip whitespace
-    field_names = field.split(input_data_delimiter)
-    for i in range(len(field_names)):
-        field_names[i] = field_names[i].replace(' ', '')
+    author_field = field.split(input_delim)
+    for i in range(len(author_field)):
+        author_field[i] = author_field[i].replace(' ', '')
 
-    # Only multi author papers
-    if len(field_names) > 1:
-        for edge_start_name in field_names:
+    # Check for multi author papers
+    if len(author_field) > 1:
+        for edge_start_name in author_field:
 
             # Get edge start and possible edge endpoints
             edge_start_name = edge_start_name.replace(' ', '')
-            name_index = field_names.index(edge_start_name)
-            other_names = field_names[:name_index] + field_names[name_index + 1:]
+            name_index = author_field.index(edge_start_name)
+            other_names = author_field[:name_index] + author_field[name_index + 1:]
 
             # Check all possible endpoints
             for edge_end_name in other_names:
@@ -79,25 +89,20 @@ for field in csv_author_fields[2:]:
                 # Strip whitespace
                 edge_end_name = edge_end_name.replace(' ', '')
 
-                # Create empty edge array for nodes
-                if edge_start_name not in graph_edges.keys():
-                    graph_edges[edge_start_name] = []
-                if edge_end_name not in graph_edges.keys():
-                    graph_edges[edge_end_name] = []
-
                 # Create edges that are nonexistent in both directions
-                if edge_end_name not in graph_edges[edge_start_name.strip()]:
-                    if edge_start_name not in graph_edges[edge_end_name]:
+                if edge_end_name not in edges[edge_start_name.strip()]:
+                    if edge_start_name not in edges[edge_end_name]:
                         graph.add_edge(edge_start_name, edge_end_name)
-                        total_edges += 1
+                        edges[edge_start_name] = edges[edge_end_name]
+                        edge_number += 1
 
 
 # Write gexf file for graph
-edges_gexf_fp = output_data_path + input_data_name + ".gexf"
+edges_gexf_fp = output_data_path + input_name + ".gexf"
 nx.write_gexf(graph, edges_gexf_fp)
 
 # Write log statements to file
-csv_log_fp = output_data_path + input_data_name + "log.txt"
-csv_log_fh = open(csv_log_fp, "w+")
-csv_log_fh.write(str(total_edges) + " author edges successfully written to " + edges_gexf_fp + " in GEXF format")
-csv_log_fh.close()
+log_filepath = output_data_path + input_name + "log.txt"
+log_filehandler = open(log_filepath, "w+")
+log_filehandler.write(str(edge_number) + " author edges successfully written to " + edges_gexf_fp + " in GEXF format")
+log_filehandler.close()
