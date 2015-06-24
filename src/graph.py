9f5b0f42783267afdb4c44347ae436df78f3f16e
Ritwik Dutta <ritzymail@gmail.coml>
2015-06-23 20:04:12 -0400

    Working on stuff.

--- src/graph.py
+++ src/graph.py
@@ -9,8 +9,8 @@ import networkx as nx  # Graph creation and GEXP creation
 # Global variables
 metadata = []  # CSV metadata
 author_csv = []  # Author fields
-authors = []  # Author names
-edges = {}  # Graph edges
+# Create graph object
+graph = nx.DiGraph()
 
 # CLI args
 input_path = cli_args[1]
@@ -44,57 +44,36 @@ for filename in input_files:
 
             # Save metadata
             metadata.append(record)
-            author_field = record[1].split(input_dlm)
-
+            field = record[1].split(input_dlm)
             # Parse names
-            for i in range(len(author_field)):
+            for i in range(len(field)):
 
                 # Strip whitespace
-                author_field[i] = author_field[i].replace(' ', '')
+                field[i] = field[i].replace(' ', '')
 
-                # Create node and empty edge set
-                if author_field[i] not in authors:
-                    authors.append(author_field[i])
-                    edges[author_field[i]] = []
+                graph.add_node(field[i], id=graph.number_of_nodes()+1)
 
             # Save field
-            author_csv.append(author_field)
+            author_csv.append(field)
 
 # Remove fluff from fields
 author_csv = author_csv[2:]
 
-# Create graph object
-graph = nx.DiGraph()
-
 # Create and count edges between co-authors
-edge_number = 0
 for field in author_csv:
 
     # Check for multi author papers
-    if len(author_field) > 1:
-        for edge_start_name in author_field:
+    if len(field) > 1:
+        for edge_start_name in field:
 
             # Get edge start and possible edge endpoints
             edge_start_name = edge_start_name.replace(' ', '')
-            name_index = author_field.index(edge_start_name)
-            other_names = author_field[:name_index] + author_field[name_index + 1:]
+            name_index = field.index(edge_start_name)
+            other_names = field[:name_index] + field[name_index + 1:]
 
             # Check all possible endpoints
             for edge_end_name in other_names:
-
-                # Strip whitespace
-                edge_end_name = edge_end_name.replace(' ', '')
-
-                # Check for edge existence
-                if edge_end_name not in edges[edge_start_name.strip()]:
-
-                    # Check for edge in other direction
-                    if edge_start_name not in edges[edge_end_name]:
-                        # Create edge and add to graph and dict
-                        graph.add_edge(edge_start_name, edge_end_name)
-                        edges[edge_start_name] = edges[edge_end_name]
-                        edge_number += 1
-
+                graph.add_edge(edge_start_name, edge_end_name)
 
 # Write gexf file for graph
 edges_gexf_path = output_path + input_name + ".gexf"
@@ -103,6 +82,6 @@ nx.write_gexf(graph, edges_gexf_path)
 # Write log statements to file
 log_path = output_path + input_name + "log.txt"
 log_handler = open(log_path, "w+")
-log_msg = str(edge_number) + " author edges successfully written to " + edges_gexf_path + " in GEXF format"
+log_msg = str(graph.number_of_edges()) + " author edges successfully written to " + edges_gexf_path + " in GEXF format"
 log_handler.write(log_msg)
 log_handler.close()
