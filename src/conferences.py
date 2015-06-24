f1461292355d433bbe98fcfd028dd7ceb9c54c94
Ritwik Dutta <ritzymail@gmail.coml>
2015-06-24 14:37:19 -0400

    Commenting and cleaning conference and year autogenerator.

--- src/conferences.py
+++ src/conferences.py
@@ -3,20 +3,44 @@ __author__ = 'Ritwik Dutta'
 from os import listdir, system
 from time import time
 
+# Set input path for files
 conferences_global_path = 'input/Conferences/'
 
+# get names of all categories in input directory
 conference_categories = listdir(conferences_global_path)
+
+# Get start time
 total_start_time = time()
+
 for category in conference_categories:
+
+    # Get full path to category
     conference_category_path = conferences_global_path + category + '/'
+
+    # Get list of conferences in category
     conference_names = listdir(conference_category_path)
+
     for name in conference_names:
+
+        # Get full path to conference
         conference_name_path = conference_category_path + name + '/'
+
+        # Start timing individual run
         time_start = time()
+
+        # Run graph generation on conference directory
         system("python graph.py " + conference_name_path + ' output/Conferences/ ";"')
+
+        # Finish timing individual run
         time_end = time()
         time_elapsed = time_end - time_start
+
+        # Print output message
         print "Graph of " + name + "\t generated in " + str(time_elapsed) + " seconds."
+
+# Get total time elapse
 total_end_time = time()
 total_elapsed_time = total_end_time - total_start_time
+
+# Print final output message
 print "All conference graphs generated in " + str(total_elapsed_time) + " seconds."
--- src/years.py
+++ src/years.py
@@ -3,18 +3,36 @@ __author__ = 'Ritwik Dutta'
 from os import listdir, system
 from time import time
 
-conferences_global_path = 'input/Years/'
+# Set input path for files
+years_global_path = 'input/Years/'
 
-conference_years = listdir(conferences_global_path)
+# Get names of all years in input directory
+conference_years = listdir(years_global_path)
+
+# Get start time
 total_start_time = time()
+
 for year in conference_years:
-    year_path = conferences_global_path + year + '/'
+
+    # Get full path to year
+    year_path = years_global_path + year + '/'
+
+    # Start timing individual run
     time_start = time()
+
+    # Run graph generation on year directory
     system("python graph.py " + year_path + ' output/Years/ ";" ' + year)
+
+    # Finish timing individual run
     time_end = time()
     time_elapsed = time_end - time_start
+
+    # Print output message
     print "Graph of " + year + "\t generated in " + str(time_elapsed) + " seconds."
 
+# Get total time elapsed
 total_end_time = time()
 total_elapsed_time = total_end_time - total_start_time
+
+# Print final output message
 print "All year graphs generated in " + str(total_elapsed_time) + " seconds."
